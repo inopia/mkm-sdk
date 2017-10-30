ca80c0f1059ac98dc126cb7873012bfe732b0724
Seppo Erviälä <seppo.erviala@iki.fi>
2017-10-31 01:20:57 +0200

    Deal with data==None

--- mkmsdk/resolvers.py
+++ mkmsdk/resolvers.py
@@ -38,9 +38,10 @@ class SimpleResolver(object):
                 assert key in params, "{key} is not parameter list {list}".format(key=key, list=params)
 
         # MKMAPI is weird like that
-        for key, value in data.items():
-            if value in [True, False]:
-                data[key] = str(value).lower()
+        if type(data) is dict:
+            for key, value in data.items():
+                if value in [True, False]:
+                    data[key] = str(value).lower()
 
         try:
             url = url.format(**kwargs)
